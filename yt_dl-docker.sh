ab4a7de124a76cbd8ffd35065cf3fd8d3a2432b4
Bartixxx <mcbplay1@gmail.com>
2020-05-17 16:40:32 +0200

    fix

--- yt_dl-docker.sh
+++ yt_dl-docker.sh
@@ -1,1 +1,1 @@
-youtube-dl -a $1 -f bestvideo+bestaudio -o "Pobrane/%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'
+youtube-dl $1 -f bestvideo+bestaudio -o "Pobrane/%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'

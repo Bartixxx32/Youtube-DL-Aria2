97f9cd8fdd2f6b4359a4cea3ab840eb7dd6f1101
Bartixxx32 <mcbplay1@gmail.com>
2021-06-02 02:22:02 +0200

    Update yt_dl-docker.sh

--- yt_dl-docker.sh
+++ yt_dl-docker.sh
@@ -1,1 +1,1 @@
-yt-dlp -ciw $1 -f bestvideo+bestaudio -o "Downloads/%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'
+yt-dlp -ciw $1 -o "Downloads/%(title)s.%(ext)s" --external-downloader aria2c --external-downloader-args '-c -j 10 -x 3 -s 3 -k 1M'
